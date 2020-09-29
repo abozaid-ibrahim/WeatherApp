3a28d5780e61ae35295ffaa8166c989431db9f79
abuzeid <abuzeid7@gmail.com>
2020-09-29 17:43:23 +0200

    Add network module

---
+++ WeatherApp/Modules/Network/APIConstants.swift
@@ -0,0 +1,30 @@
+//
+//  APIConstants.swift
+//  Marvel
+//
+//  Created by abuzeid on 22.09.20.
+//  Copyright © 2020 abuzeid. All rights reserved.
+//
+import CryptoKit
+import Foundation
+struct APIConstants {
+    static let baseURL = "http://gateway.marvel.com/"
+    static let imageBaseUrl = "https://image.tmdb.org/t/p/"
+    static let publicKey = "651883284af1c1f6b2e73a9c8df65973"
+    static let privateKey = "e76612a1072d481cd5f5b583bd30f70e3e9c4bd7"
+}
+
+struct RequestAuth {
+    var api: (ts: String, hash: String) {
+        let date = String(Date().timeIntervalSince1970)
+        let hash = MD5(string: date + APIConstants.privateKey + APIConstants.publicKey)
+        return (ts: date, hash: hash)
+    }
+
+    private func MD5(string: String) -> String {
+        let digest = Insecure.MD5.hash(data: string.data(using: .utf8) ?? Data())
+        return digest.map {
+            String(format: "%02hhx", $0)
+        }.joined()
+    }
+}
